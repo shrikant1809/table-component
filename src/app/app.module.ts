f2e868eca37f0b1f6d3aa957efce703d352d8766
shrikant patil <98108073+shrikant1809@users.noreply.github.com>
2023-07-20 12:22:24 +0530

    Your commit message here

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
 
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { TableComponent } from './table/table.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     TableComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router'; // Import RouterModule
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, TableComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule, // Add RouterModule to the imports
+    AppRoutingModule, // Add your AppRoutingModule if you have one
+    // Other modules
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/table/table.component.ts
@@ -0,0 +1,123 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-table',
+//   templateUrl: './table.component.html',
+//   styleUrls: ['./table.component.css']
+// })
+// export class TableComponent {
+
+// }
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  role: string;
+}
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css'],
+})
+export class TableComponent implements OnInit {
+  users!: Observable<User[]>;
+  displayedUsers: User[];
+  selectedRows: Set<string> = new Set<string>();
+  searchQuery: string = '';
+  currentPage: number = 1;
+  itemsPerPage: number = 10;
+
+  constructor(private http: HttpClient) {
+    this.displayedUsers = [];
+  }
+
+  ngOnInit(): void {
+    this.users! = this.http
+      .get<User[]>(
+        'https://excelerate-profile-dev.s3.ap-south-1.amazonaws.com/1681980949109_users.json'
+      )
+      .pipe(
+        map((users) => users.sort((a, b) => parseInt(a.id) - parseInt(b.id)))
+      );
+
+    this.users!.subscribe(
+      (users) => (this.displayedUsers = users.slice(0, this.itemsPerPage))
+    );
+  }
+
+  updatePagination(): void {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    this.displayedUsers = this.filterUsers().slice(
+      startIndex,
+      startIndex + this.itemsPerPage
+    );
+  }
+
+  filterUsers(): User[] {
+    const users: User[] = [];
+    this.users!.subscribe((data) => users.push(...data));
+
+    return users.filter((user) => {
+      return (
+        user.name.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+        user.email.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+        user.role.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    });
+  }
+
+  onPageChange(page: number): void {
+    this.currentPage = page;
+    this.updatePagination();
+  }
+  getPaginationArray(): number[] {
+    const totalPages = Math.ceil(this.filterUsers().length / this.itemsPerPage);
+    return Array.from({ length: totalPages }, (_, index) => index + 1);
+  }
+
+  onSearchChange(query: string): void {
+    this.searchQuery = query;
+    this.currentPage = 1;
+    this.updatePagination();
+  }
+
+  toggleSelectAll(): void {
+    if (this.selectedRows.size === this.displayedUsers.length) {
+      this.selectedRows.clear();
+    } else {
+      this.displayedUsers.forEach((user) => this.selectedRows.add(user.id));
+    }
+  }
+
+  isRowSelected(id: string): boolean {
+    return this.selectedRows.has(id);
+  }
+
+  toggleSelectRow(id: string): void {
+    if (this.selectedRows.has(id)) {
+      this.selectedRows.delete(id);
+    } else {
+      this.selectedRows.add(id);
+    }
+  }
+
+  deleteSelectedRows(): void {
+    const users: User[] = [];
+    this.users!.subscribe((data) => users.push(...data));
+
+    const filteredUsers = users.filter(
+      (user) => !this.selectedRows.has(user.id)
+    );
+    this.users! = new Observable<User[]>((subscriber) =>
+      subscriber.next(filteredUsers)
+    );
+    this.selectedRows.clear();
+    this.updatePagination();
+  }
+}
